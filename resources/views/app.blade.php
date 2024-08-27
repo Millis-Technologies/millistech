a3af53111fac531a9f9c1089617ce0109b635f8b
Michael Millis <mmillis@pm.me>
2024-08-26 21:37:30 -0700

    fix title

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,13 +4,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>{{ config('app.name') }}</title>
 
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
-
 </head>
 <body>
 <!--================ Start Header Area =================-->
